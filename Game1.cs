74f49128924a0a6a2fc49c14df1f5d896bb148de
SayedAmir69 <sayedabulfazl.hossaini@elev.ga.lbs.se>
2026-04-24 13:51:26 +0200

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Boss_Projekt
+{
+    public class Game1 : Game
+    {
+        public Vector2 Position;
+        private int Health = 100;
+
+        private Texture2D _pixel;
+        private Rectangle _bounds;
+        private Color _color = Color.Red;
+       
+    }
+}
